e068674c94bb4e5b43564409458e9a740bebbfaf
hibaoui <imane.hibaoui@etu.u-paris.fr>
2025-04-17 12:47:03 +0200

    Import du squelette depuis idf-mobilite-net

---
+++ src/test/java/fr/u_paris/gla/project/AppTest.java
@@ -0,0 +1,14 @@
+package fr.u_paris.gla.project;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/** Unit test for simple App. */
+class AppTest {
+    /** Rigorous Test :-) */
+    @Test
+    void testPlaceholder() {
+        assertTrue(true, "It should be true that true is true...");
+    }
+}
